32847004e0d7de4cc35f5018c1a760b4efe35f8b
jfsackett <joseph@sackett.com>
2013-11-19 23:03:55 -0600

    Comments.

--- BP/src/com/sackett/reify/bp/view/BPSurface.java
+++ BP/src/com/sackett/reify/bp/view/BPSurface.java
@@ -1,3 +1,20 @@
+/*
+    Bin Packing System
+    Copyright (C) 2013  Sackett Inc.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.sackett.reify.bp.view;
 
 import java.awt.Graphics;
@@ -16,13 +33,13 @@ public class BPSurface extends JPanel {
 	
 	/** The Bin to be rendered. */
 	Bin bin;
-	BPGraphicsRenderer bpGraphicsRenderer;
-    
+
     public BPSurface(Bin bin) {
 		super();
 		this.bin = bin;
 	}
 
+    /** Graphically render the bin and packed items. */
 	private void doDrawing(Graphics graphics) {
         Graphics2D graphics2d = (Graphics2D) graphics;
     	BPGraphicsRenderer bpGraphicsRenderer = new BPGraphicsRenderer(graphics2d, 50, 50, this.getHeight());
@@ -30,6 +47,7 @@ public class BPSurface extends JPanel {
     	bpGraphicsRenderer.visit(bin);
     } 
 
+	/** Paint the component. */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
